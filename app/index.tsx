83ecc3d951fd64eb816f936b23499b4c90c83832
Natalie <natchc@gmail.com>
2024-07-20 20:55:13 +0100

    installed tailwind

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center bg-gray-300">
+      <Text className="text-2xl font-bold">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from PIL import Image
 import pytesseract
-text= pytesseract.image_to_string(Image.open('2.png'))
+text= pytesseract.image_to_string(Image.open('5.jpg'))
 lines = text.split('\n')
 for line in lines:
     if "£" in line:
         print(line)
+
+
